ec652c8900b75bb046ca2b0ff3e44434e7454039
Mikel Seara <mikelseara11@gmail.com>
2021-04-25 11:26:52 +0200

    bootstrap personalizado

---
+++ Programacion web en entorno servidor/proyecto/includes/corousel.php
@@ -0,0 +1,31 @@
+<div id="carouselExampleIndicators" style="margin-bottom: 20px; max-height: 690px;" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://m.media-amazon.com/images/M/MV5BNjU5MTg4MDUyNF5BMl5BanBnXkFtZTgwNTA3MTg5MTE@._V1_.jpg" style="margin:auto;" class="d-block w-50" alt="...">
+      <div class="carousel-caption d-none d-md-block" style="-webkit-text-stroke: 1px black;">
+        <h4>First slide label</h4>
+        <h5>Some representative placeholder content for the first slide.</h5>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://m.media-amazon.com/images/M/MV5BMjI0MDM5MjkzNF5BMl5BanBnXkFtZTgwODI4OTIwMjE@._V1_.jpg" style="margin:auto;" class="d-block w-50" alt="...">
+      <div class="carousel-caption d-none d-md-block" style="-webkit-text-stroke: 1px black;">
+      <h4>First slide label</h4>
+        <h5>Some representative placeholder content for the first slide.</h5>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
--- Programacion web en entorno servidor/proyecto/index.php
+++ Programacion web en entorno servidor/proyecto/index.php
@@ -9,8 +9,10 @@
     <?php include('includes/inc_nav.php') ?>
     <div id="ms-main">
         <div id="ms-content">
-            <div id="ms-content-flex">
-                <div class="card" style="width: 18rem;">
+            <?php include('includes/corousel.php')?>
+            <span id="titulo" class="badge bg-primary" ><h5>Películas</h5></span>
+            <div id="ms-film-flexbox">
+                <div class="card" style="width: 12rem;">
                     <img src="img/peli1.jpg" class="img-fluid" alt="pelic">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -18,7 +20,7 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
-                <div class="card" style="width: 18rem;">
+                <div class="card" style="width: 12rem;">
                     <img src="img/peli2.jpg" class="img-fluid" alt="pelic">
                     <div class="card-body">
                         <h5 class="card-title">Card title</h5>
@@ -26,12 +28,46 @@
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
+                <div class="card" style="width: 12rem;">
+                    <img src="img/peli3.jpg" class="img-fluid" alt="pelic">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 12rem;">
+                    <img src="img/peli4.jpg" class="img-fluid" alt="pelic">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 12rem;">
+                    <img src="img/peli5.jpg" class="img-fluid" alt="pelic">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 12rem;">
+                    <img src="img/peli6.jpg" class="img-fluid" alt="pelic">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
+            <!-- fin flexbox -->
 
+            <span class="badge bg-primary"><h4>Películas</h4></span>
         </div>
     </div>
     <footer>
-        
+        <p>&copy;Mikel Seara | Proyecto Desarrollo Web</p>
     </footer>
 </body>
 
